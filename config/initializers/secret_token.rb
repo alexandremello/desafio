929f439b922293179a2d77a22b2ab8aba64ab297
Alexandre Mello <alexandre.rmello@gmail.com>
2013-12-06 21:51:31 -0200

    concluido style do login e layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Desafio::Application.config.secret_key_base = '069b00c350f2f17da76711c6c7bb09464e5c979960618b67a2387029f91b4b55386be8e43fcd2f491528d75e08a6832c49e20895b06f3a1cbe39daddcc6d0970'
